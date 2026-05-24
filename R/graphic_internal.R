e003c836a27546624f6b874f328741a67e8351c2
s3rdia <s3rdia@web.de>
2026-05-24 21:43:46 +0200

    qol 1.4.0 - v29
    * Moved output functions to internal script.
    * Rotated value justification is now controlled by a fixed offset value instead of the justification parameters. This is because otherwise values would float away from the segments, if the values become larger.

--- R/graphic_internal.R
+++ R/graphic_internal.R
@@ -187,3 +187,223 @@ decollide_labels <- function(segment_labels,
     # Return shifted positions
     shifted_x_positions
 }
+
+
+
+###############################################################################
+# Graphic output
+###############################################################################
+#' Output Graphic To Device
+#'
+#' @description
+#' Draw or output a graphic to a desired destination.
+#'
+#' @param graphic_object The complete graphic to be drawn or exported.
+#' @param dimensions qol package dimensions options.
+#' @param fine_tuning The list of fine tuning parameters.
+#' @param output qol package output options.
+#'
+#' @return
+#' Returns the input graphic object.
+#'
+#' @noRd
+output_graphic <- function(graphic_object,
+                           dimensions,
+                           fine_tuning,
+                           output,
+                           by_info = NULL){
+    # Early exit if all graphics should be exported as one file containing the
+    # whole grid.
+    if (output[["by_as_grid"]]){
+        if (interactive()){
+            grid::grid.draw(graphic_object)
+        }
+
+        return(invisible(graphic_object))
+    }
+
+    # Check if only save path or file name is specified. If only one is specified
+    # print a note. Otherwise the file would not be saved and opened without a
+    # hint to why the file wasn't saved.
+    if (is.null(output[["save_path"]]) + is.null(output[["file"]]) == 1){
+        if (is.null(output[["save_path"]])){
+            print_message("NOTE", c("No save path specified. Both save path and file name with extension",
+                                    "need to be specified in the global options or style parameter for",
+                                    "the file to be saved. File won't be saved."))
+        }
+        else{
+            print_message("NOTE", c("No file name specified. Both save path and file name with extension",
+                                    "need to be specified in the global options or style parameter for",
+                                    "the file to be saved. File won't be saved."))
+        }
+    }
+
+    # First check if full file path is provided. If not only draw graphic in plot window.
+    if (is.null(output[["save_path"]]) || is.null(output[["file"]])){
+        if (interactive()){
+            grid::grid.draw(graphic_object)
+        }
+    }
+    else{
+        # If save path doesn't exist, just draw graphic in plot window.
+        if (!file.exists(output[["save_path"]])){
+            print_message("WARNING", "Path does not exist: ", output[["save_path"]])
+
+            if (interactive()){
+                grid::grid.draw(graphic_object)
+            }
+        }
+        # Save file
+        else{
+            # Get file extension to determine output format
+            extension <- tolower(tools::file_ext(output[["file"]]))
+            filename  <- tools::file_path_sans_ext(output[["file"]])
+
+            if (!extension %in% c("png", "svg", "jpeg", "jpg", "bmp", "tiff")){
+                print_message("WARNING", c("Filetype '[extension]' not supported, 'png' will be used. Valid filetypes are:",
+                                           "png, svg, jpeg, jpg, bmp, tiff"), extension = extension)
+
+                extension <- "png"
+            }
+
+            # If there is no by info, the file name is already provided through the option
+            if (is.null(by_info)){
+                filename <- paste0(filename, ".", extension)
+            }
+            # If this function is called via the by variable loop, then add the expression
+            # information to the file name.
+            else{
+                filename <- paste0(filename, "_", by_info, ".", extension)
+            }
+
+            # Put all output functions into a list to call them dynamically
+            devices <- list(png  = grDevices::png,
+                            svg  = grDevices::svg,
+                            jpeg = grDevices::jpeg,
+                            jpg  = grDevices::jpeg,
+                            bmp  = grDevices::bmp,
+                            tiff = grDevices::tiff)
+
+            device_function <- devices[[extension]]
+
+            # Store the base arguments for the output functions
+            arguments <- list(filename = paste0(output[["save_path"]], "/", filename),
+                              width    = dimensions[["graphic_width"]]  / fine_tuning[["cm_to_inch_factor"]],
+                              height   = dimensions[["graphic_height"]] / fine_tuning[["cm_to_inch_factor"]])
+
+            # Add format specific arguments based on file extension
+            if (extension %in% c("png", "jpeg", "jpg", "bmp", "tiff")){
+                arguments[["units"]] <- "in"
+                arguments[["res"]]   <- output[["resolution"]]
+            }
+
+            # Export image and draw to plot view
+            do.call(device_function, arguments)
+
+            # Draw for physical output
+            grid::grid.draw(graphic_object)
+
+            grDevices::dev.off()
+
+            # Draw visible in plot view
+            grid::grid.draw(graphic_object)
+        }
+    }
+
+    invisible(graphic_object)
+}
+
+
+#' @noRd
+output_grid <- function(dimensions,
+                        fine_tuning,
+                        output){
+    # Early exit if all graphics should be exported as single files
+    if (!output[["by_as_grid"]]){
+        return(invisible(NULL))
+    }
+
+    # Check if only save path or file name is specified. If only one is specified
+    # print a note. Otherwise the file would not be saved and opened without a
+    # hint to why the file wasn't saved.
+    if (is.null(output[["save_path"]]) + is.null(output[["file"]]) == 1){
+        if (is.null(output[["save_path"]])){
+            print_message("NOTE", c("No save path specified. Both save path and file name with extension",
+                                    "need to be specified in the global options or style parameter for",
+                                    "the file to be saved. File won't be saved."))
+        }
+        else{
+            print_message("NOTE", c("No file name specified. Both save path and file name with extension",
+                                    "need to be specified in the global options or style parameter for",
+                                    "the file to be saved. File won't be saved."))
+        }
+    }
+
+    # First check if full file path is provided. If not return.
+    if (is.null(output[["save_path"]]) || is.null(output[["file"]])){
+        return(invisible(NULL))
+    }
+    else{
+        # If save path doesn't exist, abort.
+        if (!file.exists(output[["save_path"]])){
+            print_message("WARNING", "Path does not exist: ", output[["save_path"]])
+
+            return(invisible(NULL))
+        }
+        # Save file
+        else{
+            print_step("MAJOR", "Saving to file")
+
+            # Get file extension to determine output format
+            extension <- tolower(tools::file_ext(output[["file"]]))
+            filename  <- tools::file_path_sans_ext(output[["file"]])
+
+            if (!extension %in% c("png", "svg", "jpeg", "jpg", "bmp", "tiff")){
+                print_message("WARNING", c("Filetype '[extension]' not supported, 'png' will be used. Valid filetypes are:",
+                                           "png, svg, jpeg, jpg, bmp, tiff"), extension = extension)
+
+                extension <- "png"
+            }
+
+            # Reconstruct file name
+            filename <- paste0(filename, ".", extension)
+
+            # Put all output functions into a list to call them dynamically
+            devices <- list(png  = grDevices::png,
+                            svg  = grDevices::svg,
+                            jpeg = grDevices::jpeg,
+                            jpg  = grDevices::jpeg,
+                            bmp  = grDevices::bmp,
+                            tiff = grDevices::tiff)
+
+            device_function <- devices[[extension]]
+
+            # Store the base arguments for the output functions
+            arguments <- list(filename = paste0(output[["save_path"]], "/", filename),
+                              width    = dimensions[["graphic_width"]]  / fine_tuning[["cm_to_inch_factor"]],
+                              height   = dimensions[["graphic_height"]] / fine_tuning[["cm_to_inch_factor"]])
+
+            # Add format specific arguments based on file extension
+            if (extension %in% c("png", "jpeg", "jpg", "bmp", "tiff")){
+                arguments[["units"]] <- "in"
+                arguments[["res"]]   <- output[["resolution"]]
+            }
+
+            # Grab the entire grid which is drawn in the plot view as gTree
+            grid_object <- grid::grid.grab(wrap.grobs = TRUE)
+
+            # Export image and draw to plot view
+            do.call(device_function, arguments)
+
+            # Draw for physical output
+            grid::grid.draw(grid_object)
+
+            grDevices::dev.off()
+
+            # Draw visible in plot view
+            grid::grid.draw(grid_object)
+        }
+    }
+
+    invisible(grid_object)
+}
